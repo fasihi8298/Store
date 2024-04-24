e11267b2ead87c8afbdb8fe4291bbc2cd82edce6
fasihi8298 <153727111+fasihi8298@users.noreply.github.com>
2024-04-24 04:44:39 -0700

    profile updated

--- admin/profile.php
+++ admin/profile.php
@@ -25,6 +25,8 @@ $row = mysqli_fetch_assoc($exists);
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Dashboard - Home</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <!-- css-links include -->
     <?php require_once("./includes/css-links.php") ?>
 
@@ -44,19 +46,19 @@ $row = mysqli_fetch_assoc($exists);
         <div class="row page-titles mx-0">
             <div class="col p-md-0">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="./add_user.php"><button class="btn btn-primary">Add users</button></a></li>
-                    <li class="breadcrumb-item "><a href="./view_user.php"><button class="btn btn-success">View User</button></a></li>
+                    <li class="breadcrumb-item"><a href="./add_user.php"><button class="btn btn-success text-white">Add users</button></a></li>
+                    <li class="breadcrumb-item "><a href="./view_user.php"><button class="btn btn-danger">View User</button></a></li>
                 </ol>
             </div>
         </div>
-        <div class="container-fluid mt-3">
+        <div class="container-fluid ">
 
             <div class="row">
                 <div class="col-lg-10">
                     <div class="card">
                         <div class="card-body">
-                            <div class="media align-items-center mb-4">
-                                <img class="mr-3" src="./image/avatar/11.png" width="80" height="80" alt="">
+                            <div class="media align-items-center ">
+                                <img class="mr-3 rounded-circle" src="./images/profile.png" width="130px" height="130px" alt="">
                                 <div class="media-body ms-5">
                                     <h3 class="mb-0"><?= $row['name'] ?></h3>
                                     <p class="text-muted mb-0"><?= $row['role'] ?></p>
@@ -66,11 +68,13 @@ $row = mysqli_fetch_assoc($exists);
 
 
 
-                            <h4>About Me</h4>
+                            <h4 class="mt-3">About Me</h4>
                             <p class="text-muted">Hi, I'm <span><?= $row['name'] ?></span></p>
                             <ul class="card-profile__info">
-                                <li class="mb-1"><strong class="text-dark mr-4">Mobile</strong> <span>03058659014</span></li>
-                                <li><strong class="text-dark mr-4">Email</strong> <?= $row['email'] ?></span></li>
+                                <li class="mb-1"><strong class="text-dark mr-4">Mobile</strong><?= $row['mobile'] ?></span></li><br>
+                                <li><strong class="text-dark mr-4">Email</strong> <?= $row['email'] ?></span></li><br>
+                                <li><strong class="text-dark mr-1">Address</strong> <?= $row['address'] ?></span></li>
+
                             </ul>
                         </div>
                     </div>
@@ -83,46 +87,6 @@ $row = mysqli_fetch_assoc($exists);
 
 
     </div>
-
-    <div class="content-body">
-        <div class="row page-titles mx-0">
-            <div class="col p-md-0">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="./add_user.php"><button class="btn btn-primary">Add users</button></a></li>
-                    <li class="breadcrumb-item "><a href="./view_user.php"><button class="btn btn-success">View User</button></a></li>
-                </ol>
-            </div>
-        </div>
-        <div class="container-fluid mt-3">
-
-            <div class="row">
-                <div class="col-lg-10">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="media align-items-center mb-4">
-                                <img class="mr-3" src="./images/profile.png" width="80" height="80" alt="">
-                                <div class="media-body ms-5">
-                                    <h3 class="mb-0"><?= $row['name'] ?></h3>
-                                    <p class="text-muted mb-0"><?= $row['role'] ?></p>
-                                </div>
-                            </div>
-
-
-
-
-                            <h4>About Me</h4>
-                            <p class="text-muted">Hi, I'm <span><?= $row['name'] ?></span>, has been the industry standard dummy text ever since the 1500s.</p>
-                            <ul class="card-profile__info">
-                                <li class="mb-1"><strong class="text-dark mr-4">Mobile</strong> <span>01793931609</span></li>
-                                <li><strong class="text-dark mr-4">Email</strong> <?= $row['email'] ?></span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                </form>
-            </div>
-        </div>
 
 
 
